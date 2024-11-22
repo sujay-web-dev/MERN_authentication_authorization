fc68ea9d5f1b12951e053bd05a9f0ea66004a8bd
Sujay W <sujaywebdev@gmail.com>
2024-11-22 12:22:14 +0530

    some more initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
