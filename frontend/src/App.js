ca5f5a82707eb719805f6670d46e5e1a83b91d0f
Sujay W <sujaywebdev@gmail.com>
2024-11-23 19:48:31 +0530

    Protecting React Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
 import Signup from './pages/SignUp'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
+            <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
-            <Route 
+            <Route
               path="/login"
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
-            <Route 
+            <Route
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />} 
             />
           </Routes>
         </div>
